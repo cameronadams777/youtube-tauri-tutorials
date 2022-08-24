1073181d50129d1cb8f33458c62f15a337793977
Cameron Adams <cameron.adams@hey.com>
2022-08-24 01:51:46 -0500

    feat: open file explorer dialog and read file contents

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+import { open } from "@tauri-apps/api/dialog";
+import { readTextFile } from "@tauri-apps/api/fs";
+
+const contents = ref("");
+
+const readFileContents = async () => {
+  try {
+    const selectedPath = await open({
+      multiple: false,
+    });
+    if (!selectedPath) return;
+    contents.value = await readTextFile(selectedPath as string);
+  } catch (err) {
+    console.error(err);
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p v-if="contents.length">{{ contents }}</p>
+  <button @click="readFileContents">Open File Explorer</button>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
