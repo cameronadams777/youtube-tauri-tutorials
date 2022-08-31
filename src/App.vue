a393408f1606f6610858589d0adbe47cdc9b0e0e
Cameron Adams <cameron.adams@hey.com>
2022-08-31 01:19:47 -0500

    feat: invoke rust function from client to double the value of number passed

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import { invoke } from "@tauri-apps/api/tauri";
+import { ref } from "vue";
+
+const num = ref(0);
+
+const double = async () => {
+  try {
+    const response = await invoke("double", { test: num.value });
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="form-container">
+    <input v-model="num" type="number" />
+    <button @click="double">Send Ping</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
