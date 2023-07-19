4f0b729ab1dfc22c0bcdfd102bd27f52d3d21ca5
Cameron Adams <cameron.adams@hey.com>
2023-07-19 00:40:47 -0500

    feat: add custom menubar items

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,27 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
+fn create_app_menu() -> Menu {
+    return Menu::new()
+        .add_submenu(Submenu::new(
+            "App",
+            Menu::new().add_native_item(MenuItem::Quit),
+        ))
+        .add_submenu(Submenu::new(
+            "File",
+            Menu::new()
+                .add_item(CustomMenuItem::new("new".to_string(), "New").accelerator("CmdOrCtrl+N"))
+                .add_item(CustomMenuItem::new("open".to_string(), "Open").accelerator("CmdOrCtrl+O"))
+                .add_native_item(MenuItem::Separator)
+                .add_item(CustomMenuItem::new("save".to_string(), "Save").accelerator("CmdOrCtrl+S")),
+        ));
+}
+
 fn main() {
     tauri::Builder::default()
+        .menu(create_app_menu())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
